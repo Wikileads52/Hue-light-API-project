de2bde135fa5a52476db95053f5538f6a3b56b61
Wikileads52 <138142695+Wikileads52@users.noreply.github.com>
2026-01-22 12:41:01 +0100

    Add files via upload

---
+++ get_url.py
@@ -0,0 +1,119 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#env variable
+Bridge_IP = os.getenv("Hue_Bridge_IP")
+UserName = os.getenv("Hue_UserName")
+
+
+#header
+headers= {
+    "hue-application-key": UserName,
+    "Content-Type": "application/json"
+}
+
+def url_room_selection():
+    name_rooms=[]
+    rid_rooms=[]
+    url = f"https://{Bridge_IP}/clip/v2/resource/room"
+    response = requests.get(url, headers=headers, timeout=5, verify= False)
+    if response.status_code == 200:
+        data = response.json()
+        for group in data["data"]:
+            metadata_data = group['metadata']['name']
+            name_rooms.append(metadata_data)
+            for service in group["services"]:
+                if service ["rtype"] == "grouped_light":
+                    rid_rooms.append(service["rid"])
+        for i, item in enumerate(name_rooms, start=1):
+            print(f"{i}: {item}")
+        while True:
+            try:
+                choice = int(input("> "))
+                if 1 <= choice <= len(name_rooms):
+                    selected_rooms = name_rooms[choice - 1]
+                    selected_room_rid = rid_rooms[choice - 1]
+                    print(f"You selected: {selected_rooms}")
+                    print(f"You selected: {selected_room_rid}")
+                    url = f"https://{Bridge_IP}/clip/v2/resource/grouped_light/{selected_room_rid}"
+                    break
+                else:
+                    print("Invalid selection")
+            except ValueError:
+                print("Please enter a valid number")
+        return url
+    else:
+        print (f"{response.status_code}")
+
+def url_zone_selection():
+    name_zones=[]
+    rid_zones=[]
+    url = f"https://{Bridge_IP}/clip/v2/resource/zone"
+    response = requests.get(url, headers=headers, timeout=5, verify= False)
+    if response.status_code == 200:
+        data = response.json()
+        for group in data["data"]:
+            metadata_data_name_zones = group['metadata']['name']
+            name_zones.append(metadata_data_name_zones)
+            for service in group["services"]:
+                rid_zones.append(service["rid"])
+        for i, item in enumerate(name_zones, start=1):
+            print(f"{i}: {item}")
+        while True:
+            try:
+                choice = int(input("> "))
+                if 1 <= choice <= len(name_zones):
+                    selected_zone_name = name_zones[choice - 1]
+                    selected_zone_rid = rid_zones[choice - 1]
+                    print(f"You selected: {selected_zone_name}")
+                    print(f"You selected: {selected_zone_rid}")
+                    url = f"https://{Bridge_IP}/clip/v2/resource/grouped_light/{selected_zone_rid}"
+                    break
+                else:
+                    print("Invalid selection")
+            except ValueError:
+                print("Please enter a valid number:")
+        return url
+    else:
+        print (f"{response.status_code}")
+
+def url_light_selection():
+    rid_lights=[]
+    name_lights=[]
+    url = f"https://{Bridge_IP}/clip/v2/resource/light"
+    response = requests.get(url, headers=headers, timeout=5, verify= False)
+    if response.status_code == 200:
+        data = response.json()
+        for group in data["data"]:
+            metadata_data = group['metadata']['name']
+            name_lights.append(metadata_data)
+            for owner in data['data']:
+                rid_lights_listing = owner["id"]
+                rid_lights.append(rid_lights_listing)
+        for i, item in enumerate(name_lights, start=1):
+            print(f"{i}: {item}")
+        while True:
+            try:
+                choice = int(input("> "))
+                if 1 <= choice <= len(name_lights):
+                    selected_light_name = name_lights[choice - 1]
+                    selected_light_rid = rid_lights[choice - 1]
+                    print(f"You selected: {selected_light_name}")
+                    print(f"You selected: {selected_light_rid}")
+                    url = f"https://{Bridge_IP}/clip/v2/resource/light/{selected_light_rid}"
+                    break
+                else:
+                    print("Invalid selection")
+            except ValueError:
+                print("Please enter a valid number")
+        return url
+    else:
+        print (f"{response.status_code}")
+
+if __name__ == "__main__":
+    url_room_selection()
+    url_zone_selection()
+    url_light_selection()
--- url_type_selection.py
+++ url_type_selection.py
@@ -1,11 +1,9 @@
-import requests
-import json
 import os
-from time import sleep
 from dotenv import load_dotenv
-from get_room_url import url_room_selection
-from get_zone_url import url_zone_selection
-from get_light_url import url_light_selection
+import get_url
+#from get_url import url_room_selection
+#from get_url import url_zone_selection
+#from get_url import url_light_selection
 
 load_dotenv()
 
@@ -30,23 +28,27 @@ def url_selection_function():
         "4":"Exit the application"
     }
     type_needed_list = "\n".join(f"{k}: {v}" for k, v in type_needed.items())
-    
+
+    #Implementation of the selection logic
     type_selected = (input(f"Select from the list which type of element you want to change: \n{type_needed_list}\n> "))
     if type_selected =="1":
         print(f"You selected Room: ")
         url_group_type_selected = "grouped_light"
-        selected_rooms = url_room_selection()
+        selected_rooms = get_url.url_room_selection()
+        #selected_rooms = url_room_selection()
         print(selected_rooms)
         url = selected_rooms
     elif type_selected =="2":
         print(f"You selected Zone: ")
         url_group_type_selected = "grouped_light"
-        selected_zones = url_zone_selection()
+        selected_zones = get_url.url_zone_selection()
+        #selected_zones = url_zone_selection()
         print(selected_zones)
         url = selected_zones
     elif type_selected =="3":
         url_group_type_selected = "light"
-        selected_lights = url_light_selection()
+        selected_lights = get_url.url_light_selection()
+        #selected_lights = url_light_selection()
         print(selected_lights)
         url = selected_lights
     elif type_selected =="4":
@@ -55,14 +57,11 @@ def url_selection_function():
         return None
     else:
         print("Invalid entry")
+        url_selection_function()
         return None
-    #return url_group_type_selected
     return url
+
 #execution bloc
-
 if __name__ == "__main__":
-   url = url_selection_function()
-
-#print(url)
-#url = f"https://{Bridge_IP}/clip/v2/resource/{url_group_type_selected}/{grouped_light_id}"
-#print(url)
+   while True:
+    url = url_selection_function()
